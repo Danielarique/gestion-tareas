424d08a364b068823dd2bbe8508af76355884ef6
Daniela Rique <drique@asesoftware.com>
2023-05-23 22:54:56 -0500

    Avance en validaciones

--- src/App/TareasContext/index.js
+++ src/App/TareasContext/index.js
@@ -49,21 +49,51 @@ function TareasProvider({ children }) {
     const subtareasRealizadas = item.subtareas.filter(
       (subtarea) => subtarea.realizada === false
     );
-    // if (subtareasRealizadas.length > 0) {
     acc.push({
       id: item.id,
       subtareas: subtareasRealizadas,
     });
-    //  }
     return acc;
   }, []);
   const realizarTarea = (idTarea, idSubtarea) => {
-    const newItems = [...tareas];
-    const tareaIndex = newItems.findIndex((tarea)=> tarea.id ===idTarea);
-    const subTareaIndex = newItems[tareaIndex].subtareas.findIndex((subtarea)=> subtarea.idSub === idSubtarea);
-  
-    newItems[tareaIndex].subtareas[subTareaIndex].realizada = true;
-    guardarTarea(newItems); 
+    console.log(idTarea, idSubtarea);
+
+    console.log(tareas);
+    const errores = detectarErrores(`${idTarea}.${idSubtarea}`);
+    if (errores !== "") {
+      alert(errores)
+    } else {
+      const newItems = [...tareas];
+      const tareaIndex = newItems.findIndex((tarea) => tarea.id === idTarea);
+      const subTareaIndex = newItems[tareaIndex].subtareas.findIndex(
+        (subtarea) => subtarea.idSub === idSubtarea
+      );
+      newItems[tareaIndex].subtareas[subTareaIndex].realizada = true;
+      guardarTarea(newItems);
+    }
+    // guardarTarea(newItems);
+  };
+
+  const detectarErrores = (tarea) => {
+    if (tarea !== "1.1") {
+      if (!tareas[0].subtareas[0].realizada) {
+        return "Debe finalizar la tarea #1 primero";
+      }
+
+      if (tarea === "3.2" && !tareas[2].subtareas[0].realizada) {
+        return "Debe finalizar la tarea #3.1 primero";
+      }
+
+      if (tarea === "4.1" && !tareas[2].subtareas[1].realizada) {
+        return "Debe finalizar la tarea #3.2 primero";
+      }
+
+      if (tarea === "5.2" && !tareas[4].subtareas[0].realizada) {
+        return "Debe finalizar la tarea #5.1 primero";
+      }
+    }
+
+    return "";
   };
   return (
     <TareasContext.Provider value={{ itemsRealizados, realizarTarea }}>
